b40e65031b30265dc4f49187a0066a9d1428a053
Cassandra <cassandra.tang@hotmail.com>
2024-01-07 19:46:49 +0000

    fixed colours

--- src/app/components/object-details/object-details.jsx
+++ src/app/components/object-details/object-details.jsx
@@ -45,7 +45,7 @@ const ObjectDetails = ({ data }) => {
           <Link className="text-sm" href="/">
             <button
               type="button"
-              class="py-2 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-slate-800 focus:z-10 focus:ring-1 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              class="py-2 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-slate-800 focus:z-10 focus:ring-1 focus:ring-gray-200 "
             >
               ← Back home
             </button>
