31e76bbfc222535ae8f28fc29e6cc01c2af3d6c5
Cassandra <cassandra.tang@hotmail.com>
2024-01-07 19:15:57 +0000

    added dynamic routes

--- src/app/components/content/content.jsx
+++ src/app/components/content/content.jsx
@@ -1,18 +1,18 @@
+import Link from "next/link";
 import data from "../../data.json";
 
-export default function Content({ selected }) {
+const Content = ({ selected }) => {
   const listofImages =
     selected === "all"
       ? data
       : data.filter((data) => data.type.includes(selected));
 
-  return listofImages.map((data) => {
-    return (
-      <div className="block my-12 sm:my-6">
+  return listofImages.map((data) => (
+    <div key={data.id} className="block my-12 sm:my-6">
+      <Link href="/[index]" as={`/object/${data.id}`}>
         <div className="padding-6 flex content-center items-center aspect-square bg-slate-100">
           <img
             className="object-contain aspect-square"
-            key={data.index}
             src={data.url}
             alt={data.alt}
             loading="lazy"
@@ -21,11 +21,13 @@ export default function Content({ selected }) {
         <div className="block gap-2">
           <div className="flex justify-between mt-4">
             <p className="text-sm text-slate-800">{data.title}</p>
-            <p className="text-sm text-slate-800">{data.index}</p>
+            <p className="text-sm text-slate-800">{data.id}</p>
           </div>
           <p className="text-sm text-gray-400">{data.description}</p>
         </div>
-      </div>
-    );
-  });
-}
+      </Link>
+    </div>
+  ));
+};
+
+export default Content;
